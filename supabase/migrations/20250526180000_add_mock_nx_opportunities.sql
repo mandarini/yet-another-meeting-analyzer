b4db8de66d5fc3ff2fe9820f61f4f7031b7b3348
Katerina Skroumpelou <sk.katherine@gmail.com>
2025-05-27 17:43:21 +0300

    add opportinutues

---
+++ supabase/migrations/20250526180000_add_mock_nx_opportunities.sql
@@ -0,0 +1,251 @@
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '6291a22b-bfbd-4476-867c-7b85160e3108',
+  '0ad9a7a5-0e61-4bf6-810c-3810771c1c53',
+  'Nx Agents',
+  0.9,
+  'Use Nx Agents to optimize task distribution and reduce CI time'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '50a8fe21-7a23-4154-b15f-5b3b778c6979',
+  '0cfa466f-5846-4f99-b343-56f928dfd6be',
+  'Nx',
+  0.9,
+  'Customize Nx plugin tools to streamline repetitive dev tasks'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '0b56396f-585c-4cfc-ae0d-61585ec2459e',
+  '11d67f0f-1878-41dc-9b02-4ea0f2f358e5',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '42d1a4f7-7960-430e-9217-1ab3400763d7',
+  '224ec6e1-6e46-4eb8-a77d-823c418e5a02',
+  'Nx',
+  0.9,
+  'Leverage Nx plugins to isolate and speed up TypeScript builds'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '3f0cb289-c6b6-4440-aa56-4c1d8077e707',
+  '35601c6b-a616-4316-9b9b-217cfe29c9ef',
+  'Nx',
+  0.9,
+  'Start with Nx Cloud for immediate improvements'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '1fced16a-bb04-443e-b2bf-4eecff9cfd79',
+  '4c197cbd-6f7f-4674-8a7b-1ed830db4006',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '0e75abae-7d19-4329-b1fc-d119632daf59',
+  '4ee7ae70-da8c-4877-8af9-0570fef253b9',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  'a25fb1a0-c316-4b33-b558-498ddef9de31',
+  '669fedbe-402e-420b-81c2-bf570d084bc5',
+  'Nx',
+  0.9,
+  'Use Nx generators and project references to better structure monorepo'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '87a60426-e7c6-4dd9-a42f-7ac28db166d1',
+  '7545b943-0a5a-4c89-a3f1-078b0b1ee574',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '4c8d2c00-69e6-4af6-b95f-f5d422637957',
+  '9a6900b3-36de-439d-92f8-5996a8fceb16',
+  'Nx Agents',
+  0.9,
+  'Use Nx Agents to optimize task distribution and reduce CI time'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '69493606-13bb-46b1-93ed-e858aee7c84d',
+  'b4181ab5-1115-4b68-9223-8333843aa5ad',
+  'Nx',
+  0.9,
+  'Start with Nx Cloud for immediate improvements'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '46ef69f8-255b-44c9-a2ad-be137f0874b4',
+  'be8f47ab-c799-4dfd-8731-b8b5dd340bea',
+  'Nx',
+  0.9,
+  'Customize Nx plugin tools to streamline repetitive dev tasks'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  'e2c408b8-ccad-44cb-9227-83fb9a4eb8f2',
+  'c0011e10-2ca5-4d6e-97aa-b6cde5ebbbf7',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '6fab75fe-ccd2-4479-a45b-a380dc5d29dd',
+  'c97d4420-421e-40ef-a5ba-598d31ee0781',
+  'Nx Cloud',
+  0.9,
+  'Set up Nx Cloud for caching and faster CI'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  '39c83c42-e4fa-4af1-b15f-6bc4c4b49267',
+  'd4d6cc23-cc03-4266-bb64-e11d4ae619f9',
+  'Nx',
+  0.9,
+  'Customize Nx plugin tools to streamline repetitive dev tasks'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  'de578e34-3062-428f-9a89-58fed6c3acad',
+  'dc6da4c4-b38e-4321-b1a2-603989cb9765',
+  'Nx',
+  0.9,
+  'Start with Nx Cloud for immediate improvements'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  'c0297c04-bce7-43d7-90a9-1b1dd38ad34e',
+  'ec616bd4-c553-4c78-aa71-98022e2335fb',
+  'Nx',
+  0.9,
+  'Start with Nx Cloud for immediate improvements'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO nx_opportunities (
+  meeting_id,
+  pain_point_id,
+  nx_feature,
+  confidence_score,
+  suggested_approach
+) VALUES (
+  'a8d97a20-c27a-456d-9ee0-bea841aefa42',
+  'f501046e-9c63-46cd-b3d8-ac7a20a4a1ef',
+  'Nx',
+  0.9,
+  'Upgrade Nx version to benefit from latest optimizations'
+) ON CONFLICT DO NOTHING;
